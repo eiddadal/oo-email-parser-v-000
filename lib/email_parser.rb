6e8e4e488872182c0574ab44182d5a691f0de745
Dalya Eidda <dalia.eidda@gmail.com>
2019-01-14 22:23:30 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -3,9 +3,7 @@
 # unique email addresses. The delimiters to support are commas (',')
 # or whitespace (' ').
 class EmailParser
-  @@all = []
-  
-  
+
   def parse
     self.new.split(", ")
   end 
